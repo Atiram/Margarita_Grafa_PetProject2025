02a7a0e039242845a13c030677ce93f6120ca6dd
DESKTOP-7BINQR3\User <marharyta.hrafa@innowise.com>
2025-02-17 17:23:00 +0100

    Removed lines

--- ClinicService.DAL/Data/ClinicDbContext.cs
+++ ClinicService.DAL/Data/ClinicDbContext.cs
@@ -10,7 +10,6 @@ public class ClinicDbContext : DbContext
 
     public ClinicDbContext(DbContextOptions<ClinicDbContext> options) : base(options)
     {
-        //Database.EnsureCreated();
         //if (Database.IsRelational())
         //{
         //    Database.Migrate();
