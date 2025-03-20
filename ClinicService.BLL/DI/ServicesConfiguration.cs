531e9588b1126332e13a0c4b52d19c01e2eeb360
DESKTOP-1IC0ELR\Dev <Margarita Grafa>
2025-03-20 11:37:19 +0300

    Change DI

--- ClinicService.BLL/DI/ServicesConfiguration.cs
+++ ClinicService.BLL/DI/ServicesConfiguration.cs
@@ -10,18 +10,12 @@ namespace ClinicService.BLL.DI;
 public static class ServicesConfiguration
 {
     public static void RegisterBusinessLogicServices(this IServiceCollection services, IConfiguration configuration)
-    {
-        string? eventUrl = configuration.GetSection("EventUrl").Value;
-
-        if (string.IsNullOrEmpty(eventUrl))
-        {
-            throw new ArgumentException("Section 'EventUrl' is missing or empty in configuration.");
-        }
+    {        
         services.AddAutoMapper(Assembly.GetAssembly(typeof(AppMappingProfile)));
         services.AddScoped<IDoctorService, DoctorService>()
             .AddScoped<IPatientService, PatientService>()
             .AddScoped<IAppointmentService, AppointmentService>()
-            .AddScoped<INotificationHttpClient, NotificationHttpClient>(provider => new NotificationHttpClient(eventUrl))
+            .AddScoped<INotificationHttpClient, NotificationHttpClient>()
             .RegisterDataRepositories(configuration);
     }
 }
--- ClinicService.BLL/Services/NotificationHttpClient.cs
+++ ClinicService.BLL/Services/NotificationHttpClient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using Clinic.DOMAIN;
 using ClinicService.BLL.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace ClinicService.BLL.Services;
@@ -8,13 +9,15 @@ public class NotificationHttpClient : INotificationHttpClient
 {
     private const string JsonContentType = "application/json";
     private string eventUrl;
-    public NotificationHttpClient(string eventUrl)
+    private HttpClient httpClient;
+
+    public NotificationHttpClient(IConfiguration configuration)
     {
-        this.eventUrl = eventUrl;
+        this.eventUrl = configuration.GetSection("EventUrl").Value ?? throw new ArgumentException("Section 'EventUrl' is missing or empty in configuration.");
+        httpClient = new HttpClient();
     }
     public async Task SendEventRequest(CreateEventMail createEventMail, CancellationToken cancellationToken)
     {
-        HttpClient httpClient = new HttpClient();
         var content = new StringContent(JsonConvert.SerializeObject(createEventMail), Encoding.UTF8, JsonContentType);
         HttpResponseMessage response = await httpClient.PostAsync(eventUrl, content, cancellationToken);
         response.EnsureSuccessStatusCode();
