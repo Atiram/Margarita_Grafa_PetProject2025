e0f0f4e7d11bf89f590216242439e895bab22391
DESKTOP-7BINQR3\User <marharyta.hrafa@innowise.com>
2025-03-25 11:48:00 +0100

    Add Domain

---
+++ Clinic.Domain/CreateEventMail.cs
@@ -0,0 +1,8 @@
+﻿namespace Clinic.Domain;
+public class CreateEventMail
+{
+    public string Email { get; set; } = null!;
+    public string Subject { get; set; } = "Your appointment is created";
+    public string Message { get; set; } = "Your appointment is created"!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Clinic.Domain/NotificationMessages.cs
@@ -0,0 +1,6 @@
+﻿namespace Clinic.Domain;
+public static class NotificationMessages
+{
+    public const string HostSectionMissingErrorMessage = "Section 'RabbitMqSettings:Host' is missing or empty in configuration.";
+    public const string QueueNameSectionMissingErrorMessage = "Section 'RabbitMqSettings:QueueName' is missing or empty in configuration.";
+}
