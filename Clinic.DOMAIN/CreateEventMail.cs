b5f6f7ef41472fe4791cd4fca2e5d544e32f9e59
DESKTOP-7BINQR3\User <marharyta.hrafa@innowise.com>
2025-03-25 11:39:05 +0100

    Remove files

--- Clinic.DOMAIN/CreateEventMail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Clinic.Domain;
-
-public class CreateEventMail
-{
-    public string Email { get; set; } = null!;
-    public string Subject { get; set; } = "Your appointment is created";
-    public string Message { get; set; } = "Your appointment is created"!;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
--- Clinic.Domain/NotificationMessages.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Clinic.Domain;
-public static class NotificationMessages
-{
-    public const string HostSectionMissingErrorMessage = "Section 'RabbitMqSettings:Host' is missing or empty in configuration.";
-    public const string QueueNameSectionMissingErrorMessage = "Section 'RabbitMqSettings:QueueName' is missing or empty in configuration.";
-}
