a57cd441788d2622e010ee599ce4bcbefec326e9
DESKTOP-7BINQR3\User <marharyta.hrafa@innowise.com>
2025-04-28 12:48:22 +0200

    Add Document Service

--- ClinicService.BLL/Services/DoctorService.cs
+++ ClinicService.BLL/Services/DoctorService.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Net.Http.Headers;
-using AutoMapper;
+﻿using AutoMapper;
 using Clinic.Domain;
 using ClinicService.BLL.Models;
 using ClinicService.BLL.Models.Requests;
@@ -8,31 +6,28 @@ using ClinicService.BLL.Services.Interfaces;
 using ClinicService.DAL.Entities;
 using ClinicService.DAL.Repositories.Interfaces;
 using ClinicService.DAL.Utilities.Pagination;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace ClinicService.BLL.Services;
 public class DoctorService(IDoctorRepository doctorRepository,
+    IDocumentService documentService,
     IMapper mapper,
-    IConfiguration configuration,
-    IHttpClientFactory httpClientFactory,
     ILogger<DoctorService> logger
     ) : IDoctorService
 {
-    private readonly int maxRetries = 3;
-    private readonly TimeSpan delay = TimeSpan.FromSeconds(2);
-    private const string FileServiceSectionName = "FileServiceBaseUrl";
-    private readonly string fileServiceBaseUrl = configuration.GetSection(FileServiceSectionName).Value ??
-        throw new ArgumentException(string.Format(NotificationMessages.SectionMissingErrorMessage, FileServiceSectionName));
-    private HttpClient httpClient = httpClientFactory.CreateClient("FileService");
+    //private readonly int maxRetries = 3;
+    //private readonly TimeSpan delay = TimeSpan.FromSeconds(2);
+    //private const string FileServiceSectionName = "FileServiceBaseUrl";
+    //private readonly string fileServiceBaseUrl = configuration.GetSection(FileServiceSectionName).Value ??
+    //    throw new ArgumentException(string.Format(NotificationMessages.SectionMissingErrorMessage, FileServiceSectionName));
+    //private HttpClient httpClient = httpClientFactory.CreateClient("FileService");
 
     public async Task<DoctorModel?> GetById(Guid id, CancellationToken cancellationToken)
     {
         var doctorEntity = await doctorRepository.GetByIdAsync(id, cancellationToken);
         if (doctorEntity != null)
         {
-            var fileUrl = await GetPhotoAsync(doctorEntity.Id, cancellationToken);
+            var fileUrl = await documentService.GetPhotoAsync(doctorEntity.Id, cancellationToken);
             var doctorModel = mapper.Map<DoctorModel>(doctorEntity);
 
             return doctorModel;
@@ -50,7 +45,7 @@ public class DoctorService(IDoctorRepository doctorRepository,
     public async Task<DoctorModel> CreateAsync(CreateDoctorRequest request, CancellationToken cancellationToken)
     {
         var doctorEntity = await doctorRepository.CreateAsync(mapper.Map<DoctorEntity>(request), cancellationToken);
-        await UploadPhotoAsync(doctorEntity.Id, request.Formfile, cancellationToken);
+        await documentService.UploadPhotoAsync(doctorEntity.Id, request.Formfile, cancellationToken);
         return mapper.Map<DoctorModel>(doctorEntity);
     }
 
@@ -66,8 +61,8 @@ public class DoctorService(IDoctorRepository doctorRepository,
         var doctorEntity = mapper.Map(request, doctor);
         if (request.Formfile != null && request.Formfile.Length != 0)
         {
-            await DeletePhotoAsync(doctorEntity.Id, cancellationToken);
-            await UploadPhotoAsync(doctorEntity.Id, request.Formfile, cancellationToken);
+            await documentService.DeletePhotoAsync(doctorEntity.Id, cancellationToken);
+            await documentService.UploadPhotoAsync(doctorEntity.Id, request.Formfile, cancellationToken);
         }
         var updatedDoctorEntity = await doctorRepository.UpdateAsync(doctorEntity, cancellationToken);
 
@@ -79,70 +74,8 @@ public class DoctorService(IDoctorRepository doctorRepository,
         var doctorEntity = await doctorRepository.GetByIdAsync(id, cancellationToken)
             ?? throw new Exception(string.Format(NotificationMessages.NotFoundErrorMessage, id));
 
-        await DeletePhotoAsync(doctorEntity.Id, cancellationToken);
+        await documentService.DeletePhotoAsync(doctorEntity.Id, cancellationToken);
         var doctorDeleted = await doctorRepository.DeleteAsync(id, cancellationToken);
         return doctorDeleted;
-    }
-
-    private async Task<string> GetPhotoAsync(Guid doctorId, CancellationToken cancellationToken)
-    {
-        var getFileByReferenceIdEndpoint = $"{fileServiceBaseUrl}/referenceId/{doctorId}";
-        var response = await ExecuteHttpRequestWithRetryAsync(
-            () => httpClient.GetAsync(getFileByReferenceIdEndpoint, cancellationToken),
-            nameof(GetPhotoAsync),
-            doctorId);
-        return await response.Content.ReadAsStringAsync();
-    }
-
-    private async Task UploadPhotoAsync(Guid doctorId, IFormFile? photoFile, CancellationToken cancellationToken)
-    {
-        if (photoFile == null || photoFile.Length == 0)
-        {
-            logger.LogWarning(string.Format(NotificationMessages.NotFoundErrorMessage, photoFile?.Name));
-            throw new ValidationException(string.Format(NotificationMessages.NotFoundErrorMessage, photoFile?.Name));
-        }
-
-        using var content = new MultipartFormDataContent();
-        using var streamContent = new StreamContent(photoFile.OpenReadStream());
-        streamContent.Headers.ContentType = new MediaTypeHeaderValue(photoFile.ContentType);
-        content.Add(streamContent, "file", photoFile.FileName);
-        content.Add(new StringContent(doctorId.ToString()), "referenceItemId");
-        content.Add(new StringContent("Photo"), "documentType");
-        content.Add(new StringContent($"{doctorId}.jpg"), "blobName");
-
-        await ExecuteHttpRequestWithRetryAsync(
-          () => httpClient.PostAsync(fileServiceBaseUrl, content, cancellationToken),
-          nameof(UploadPhotoAsync),
-          doctorId);
-    }
-
-    private async Task DeletePhotoAsync(Guid doctorId, CancellationToken cancellationToken)
-    {
-        var fileServiceDeleteEndpoint = $"{fileServiceBaseUrl}/referenceId22/{doctorId}";
-
-        await ExecuteHttpRequestWithRetryAsync(
-          () => httpClient.DeleteAsync(fileServiceDeleteEndpoint, cancellationToken),
-          nameof(DeletePhotoAsync),
-          doctorId);
-    }
-
-    private async Task<HttpResponseMessage> ExecuteHttpRequestWithRetryAsync(Func<Task<HttpResponseMessage>> httpRequest, string errorMessage, Guid? doctorId = null)
-    {
-        for (int i = 0; i <= maxRetries; i++)
-        {
-            try
-            {
-                var response = await httpRequest();
-                response.EnsureSuccessStatusCode();
-                return response;
-            }
-            catch (HttpRequestException ex) when (i < maxRetries)
-            {
-                logger.LogError(ex, string.Format(NotificationMessages.RetryingExecuteHttpRequestErrorMessage, i + 1, errorMessage, doctorId));
-                await Task.Delay(delay);
-            }
-        }
-        logger.LogError(string.Format(NotificationMessages.FailedExecuteHttpRequestErrorMessage, errorMessage, maxRetries));
-        throw new InvalidOperationException(string.Format(NotificationMessages.FailedExecuteHttpRequestErrorMessage, errorMessage, maxRetries));
     }
 }
---
+++ ClinicService.BLL/Services/DocumentService.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+using Clinic.Domain;
+using ClinicService.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ClinicService.BLL.Services;
+public class DocumentService(
+    IConfiguration configuration,
+    IHttpClientFactory httpClientFactory,
+    ILogger<DoctorService> logger
+    ) : IDocumentService
+{
+    private readonly int maxRetries = 3;
+    private readonly TimeSpan delay = TimeSpan.FromSeconds(2);
+    private const string FileServiceSectionName = "FileServiceBaseUrl";
+    private readonly string fileServiceBaseUrl = configuration.GetSection(FileServiceSectionName).Value ??
+        throw new ArgumentException(string.Format(NotificationMessages.SectionMissingErrorMessage, FileServiceSectionName));
+    private HttpClient httpClient = httpClientFactory.CreateClient("FileService");
+
+    public async Task<string> GetPhotoAsync(Guid doctorId, CancellationToken cancellationToken)
+    {
+        var getFileByReferenceIdEndpoint = $"{fileServiceBaseUrl}/referenceId/{doctorId}";
+        var response = await ExecuteHttpRequestWithRetryAsync(
+            () => httpClient.GetAsync(getFileByReferenceIdEndpoint, cancellationToken),
+            nameof(GetPhotoAsync),
+            doctorId);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public async Task UploadPhotoAsync(Guid doctorId, IFormFile? photoFile, CancellationToken cancellationToken)
+    {
+        if (photoFile == null || photoFile.Length == 0)
+        {
+            logger.LogWarning(string.Format(NotificationMessages.NotFoundErrorMessage, photoFile?.Name));
+            throw new ValidationException(string.Format(NotificationMessages.NotFoundErrorMessage, photoFile?.Name));
+        }
+
+        using var content = new MultipartFormDataContent();
+        using var streamContent = new StreamContent(photoFile.OpenReadStream());
+        streamContent.Headers.ContentType = new MediaTypeHeaderValue(photoFile.ContentType);
+        content.Add(streamContent, "file", photoFile.FileName);
+        content.Add(new StringContent(doctorId.ToString()), "referenceItemId");
+        content.Add(new StringContent("Photo"), "documentType");
+        content.Add(new StringContent($"{doctorId}.jpg"), "blobName");
+
+        await ExecuteHttpRequestWithRetryAsync(
+          () => httpClient.PostAsync(fileServiceBaseUrl, content, cancellationToken),
+          nameof(UploadPhotoAsync),
+          doctorId);
+    }
+
+    public async Task DeletePhotoAsync(Guid doctorId, CancellationToken cancellationToken)
+    {
+        var fileServiceDeleteEndpoint = $"{fileServiceBaseUrl}/referenceId/{doctorId}";
+
+        await ExecuteHttpRequestWithRetryAsync(
+          () => httpClient.DeleteAsync(fileServiceDeleteEndpoint, cancellationToken),
+          nameof(DeletePhotoAsync),
+          doctorId);
+    }
+
+    private async Task<HttpResponseMessage> ExecuteHttpRequestWithRetryAsync(Func<Task<HttpResponseMessage>> httpRequest, string errorMessage, Guid? doctorId = null)
+    {
+        for (int i = 0; i <= maxRetries; i++)
+        {
+            try
+            {
+                var response = await httpRequest();
+                response.EnsureSuccessStatusCode();
+                return response;
+            }
+            catch (HttpRequestException ex) when (i < maxRetries)
+            {
+                logger.LogError(ex, string.Format(NotificationMessages.RetryingExecuteHttpRequestErrorMessage, i + 1, errorMessage, doctorId));
+                await Task.Delay(delay);
+            }
+        }
+        logger.LogError(string.Format(NotificationMessages.FailedExecuteHttpRequestErrorMessage, errorMessage, maxRetries));
+        throw new InvalidOperationException(string.Format(NotificationMessages.FailedExecuteHttpRequestErrorMessage, errorMessage, maxRetries));
+    }
+}
